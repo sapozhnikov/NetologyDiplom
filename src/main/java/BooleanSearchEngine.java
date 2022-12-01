ec6c5d185343bb1ec74eb1837eb342929054e2d7
Dmitriy Sapozhnikov <aurariel@gmail.com>
2022-12-01 20:01:28 +0300

    basic search

--- src/main/java/BooleanSearchEngine.java
+++ src/main/java/BooleanSearchEngine.java
@@ -1,18 +1,55 @@
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfReader;
+import com.itextpdf.kernel.pdf.canvas.parser.PdfTextExtractor;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class BooleanSearchEngine implements SearchEngine {
-    //???
+    private Map<String, List<PageEntry>> searchIndex;
 
     public BooleanSearchEngine(File pdfsDir) throws IOException {
-        // прочтите тут все pdf и сохраните нужные данные,
-        // тк во время поиска сервер не должен уже читать файлы
+        searchIndex = new HashMap<>();
+        System.out.println("Indexing started ...");
+
+        if (!pdfsDir.isDirectory()) {
+            throw new IllegalArgumentException("Wrong directory to index");
+        }
+        String[] files = pdfsDir.list();
+
+        assert files != null;
+        for (String file : files) {
+            PdfDocument doc = new PdfDocument(new PdfReader(pdfsDir + "/" + file));
+            for (int pageNum = 0; pageNum < doc.getNumberOfPages(); pageNum++) {
+                String pageText = PdfTextExtractor.getTextFromPage(doc.getPage(pageNum + 1)).toLowerCase();
+                String[] words = pageText.split("\\P{IsAlphabetic}+");
+                Map<String, Integer> freqs = new HashMap<>();;
+                for (String word : words) {
+                    //update freqs
+                    if (!word.isEmpty()) {
+                        freqs.put(word, freqs.getOrDefault(word, 0) + 1);
+                    }
+                }
+                for (var freq : freqs.entrySet()) {
+                    List<PageEntry> entries = searchIndex.get(freq.getKey());
+                    if (entries == null) {
+                        entries = new ArrayList<>();
+                        searchIndex.put(freq.getKey(), entries);
+                    }
+                    entries.add(new PageEntry(file, pageNum + 1, freq.getValue()));
+                }
+            }
+        }
+        System.out.println("Finished indexing, " + searchIndex.size() + " unique words");
     }
 
     @Override
     public List<PageEntry> search(String word) {
-        // тут реализуйте поиск по слову
-        return Collections.emptyList();
+        return searchIndex.entrySet().stream().filter(x -> x.getKey().equals(word))
+                .map(Map.Entry::getValue)
+                .flatMap(List::stream)
+                .sorted().collect(Collectors.toList());
     }
 }
--- src/main/java/PageEntry.java
+++ src/main/java/PageEntry.java
@@ -3,5 +3,40 @@ public class PageEntry implements Comparable<PageEntry> {
     private final int page;
     private final int count;
 
-    // ???
+    public PageEntry(String pdfName, int page, int count) {
+        this.pdfName = pdfName;
+        this.page = page;
+        this.count = count;
+    }
+
+    public String getPdfName() {
+        return pdfName;
+    }
+
+    public int getPage() {
+        return page;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    @Override
+    public String toString() {
+        return "PageEntry{" +
+                "pdfName='" + pdfName + '\'' +
+                ", page=" + page +
+                ", count=" + count +
+                '}';
+    }
+
+    @Override
+    public int compareTo(PageEntry o) {
+        int result = Integer.compare(o.getCount(), count);
+        //early pages goes up in search
+        if (result == 0) {
+            result = Integer.compare(page, o.getPage());
+        }
+        return result;
+    }
 }
