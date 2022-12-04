58c9a66100348c4832fb079669081d2c20eeecbf
Dmitriy Sapozhnikov <aurariel@gmail.com>
2022-12-04 21:16:45 +0300

    server implemented

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,31 @@
-import java.io.File;
-import java.util.Arrays;
+import com.google.gson.Gson;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws Exception {
+        Gson gson = new Gson();
         BooleanSearchEngine engine = new BooleanSearchEngine(new File("pdfs"));
-        System.out.println(engine.search("бизнес количество руководитель"));
 
-        // здесь создайте сервер, который отвечал бы на нужные запросы
-        // слушать он должен порт 8989
-        // отвечать на запросы /{word} -> возвращённое значение метода search(word) в JSON-формате
+        try (ServerSocket serverSocket = new ServerSocket(8989);) {
+            while (true) {
+                try (
+                        Socket socket = serverSocket.accept();
+                        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                        PrintWriter out = new PrintWriter(socket.getOutputStream());
+                ) {
+                    String stringFromSocket = in.readLine(); //server in
+                    System.out.println("server received: " + stringFromSocket);
+                    List<PageEntry> entries = engine.search(stringFromSocket);
+                    out.println(gson.toJson(entries));
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Не могу стартовать сервер");
+            e.printStackTrace();
+        }
     }
 }
